e47a63e0bc4591766e2527d93702f6cb9168e6e6
Jun Hu <hujun@lightgogogo.com>
2023-08-23 20:28:49 +0800

    gen config

--- debug_run_tag_rank.sh
+++ debug_run_tag_rank.sh
@@ -1,2 +1,2 @@
-python generate_config_js.py
+python generate_config_js.py --start_mode debug
 uvicorn tag_rank_api:app --host=0.0.0.0 --port=12899
--- run_tag_rank.sh
+++ run_tag_rank.sh
@@ -1,1 +1,2 @@
+python generate_config_js.py --start_mode online
 uvicorn tag_rank_api:app --host=0.0.0.0 --port=12856
